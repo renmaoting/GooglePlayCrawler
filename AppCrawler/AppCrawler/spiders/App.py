f8584611ae3e6d49fd80cf290235168a89cbee2b
renmaoting <rmtustc@gmail.com>
2016-11-11 13:12:33 -0500

    now can correctly get price

--- AppCrawler/AppCrawler/spiders/App.py
+++ AppCrawler/AppCrawler/spiders/App.py
@@ -18,19 +18,21 @@ class AppSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
-        if response.url.find('reviewId') != -1: 
-            return;
+        if response.url.find('reviewId') != -1: return;
         item = AppCrawlerItem()
     
-        item["URL"] = response.url
-        item["Name"] = response.xpath('//div[@class="id-app-title"]/text()').extract()
-        item["Downloads"] = response.xpath("//div[@itemprop='numDownloads']/text()").extract()
-        item["Updated"] = response.xpath("//div[@itemprop='datePublished']/text()").extract()
-        item["Version"] = response.xpath('//div[@itemprop="softwareVersion"]/text()').extract()
-        item["Review_number"] = response.xpath("//span[@class='reviews-num']/text()").extract()
-        item["Rating"] = response.xpath("//div[@class='score']/text()").extract()
-        item["Author"] = response.xpath('//div[@itemprop="author"]/a/span/text()').extract()
-        item["Genre"] = response.xpath('//span[@itemprop="genre"]/text()').extract()
+        item["URL"] = response.url[0]
+        item["Name"] = response.xpath('//div[@class="id-app-title"]/text()').extract_first()
+        item["Downloads"] = response.xpath("//div[@itemprop='numDownloads']/text()").extract_first()
+        item["Updated"] = response.xpath("//div[@itemprop='datePublished']/text()").extract_first()
+        item["Version"] = response.xpath('//div[@itemprop="softwareVersion"]/text()').extract_first()
+        item["Review_number"] = response.xpath("//span[@class='reviews-num']/text()").extract_first()
+        item["Rating"] = response.xpath("//div[@class='score']/text()").extract_first()
+        item["Author"] = response.xpath('//div[@itemprop="author"]/a/span/text()').extract_first()
+        item["Genre"] = response.xpath('//span[@itemprop="genre"]/text()').extract_first()
+        price = response.xpath('//button[@class="price buy id-track-click id-track-impression"]/span[2]/text()').extract_first()
+        if price == u'Install': item["Price"] = 'free' 
+        else: item["Price"] = price.split()[0]
 
         yield item
 
